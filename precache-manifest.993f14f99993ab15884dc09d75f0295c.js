2bbc699418af7d696d77bfc0ad2eef736d8ff04a
Jess Mear <jessmeardev@gmail.com>
2019-10-15 07:46:42 -0700

    Updates

---
+++ precache-manifest.993f14f99993ab15884dc09d75f0295c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc86bba03a2a3d246e6cdc1cdfdeb1c",
+    "url": "/code-poetry/index.html"
+  },
+  {
+    "revision": "90951755e3f79c411c41",
+    "url": "/code-poetry/static/css/main.548efda5.chunk.css"
+  },
+  {
+    "revision": "38c8c9c0707f1f6f73d7",
+    "url": "/code-poetry/static/js/2.79d500ff.chunk.js"
+  },
+  {
+    "revision": "90951755e3f79c411c41",
+    "url": "/code-poetry/static/js/main.0ae69b33.chunk.js"
+  },
+  {
+    "revision": "c43717d8d96b8634371f",
+    "url": "/code-poetry/static/js/runtime-main.5c859ef8.js"
+  }
+]);
